5dd955e604b2ce0b6d3cfd4da2231466f7c0c785
wanglc <wlc.9424@gmail.com>
2019-03-13 14:33:56 -0700

    Update TG_plot.m

--- TG_plot.m
+++ TG_plot.m
@@ -2,11 +2,33 @@ clear
 close
 clc
 
-data = csvread('TG_data.csv',1,1);
+% Read in csv file
+data = csvread('TG_data.csv',1,0);
 temp = zeros(17, 17);
 
-for i = 0:16
-	row = (50*i)+6; % 6, 56, 106, ..., 806 
-    temp(i+1,:) = data(row, :);
+% Plot certain frame
+frame = 1;
+time = data(1, 1);
+for line = 1:17
+    row = (50*(line-1))+frame; % 6, 56, 106, ..., 806 
+    temp(line,:) = data(row, 2:end);
 end
+subplot(1,2,1)
 pcolor(temp); axis square
+title( ['Frame: ', num2str(frame), '. At ', num2str(time), ' s'] )
+colorbar
+
+% Average out all frames
+temp_all = zeros(17, 17);
+time_all = data(50, 1);
+for frame = 1:50
+    for line = 1:17
+        row = (50*(line-1))+frame; % 6, 56, 106, ..., 806 
+        temp(line,:) = data(row, 2:end);
+    end
+    temp_all = temp_all + temp;
+end
+subplot(1,2,2)
+pcolor(temp_all./50); axis square
+title(['Average out all 50 frames. ', 'In ', num2str(time_all), ' s'])
+colorbar
